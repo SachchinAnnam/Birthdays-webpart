e505416fec8db8d1ea78611bb02a84d0f640be0b
João <joao.j.mendes@outlook.com>
2018-12-08 19:31:11 +0000

    commit

---
+++ src/webparts/Birthdays/loc/mystrings.d.ts
@@ -0,0 +1,11 @@
+declare interface IHappyBirdthayWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  NumberUpComingDaysLabel: string;
+}
+
+declare module 'HappyBirdthayWebPartStrings' {
+  const strings: IHappyBirdthayWebPartStrings;
+  export = strings;
+}
